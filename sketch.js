164b3e400a928757dbc1f00ec78566571a244152
alvin87 <alvinwidodo87@gmail.com>
2024-07-10 17:48:39 +0700

    update

--- sketch.js
+++ sketch.js
@@ -1,54 +1,28 @@
+let gantiWarna = false;
+function setup() {
+  createCanvas(500, 500, WEBGL);
+}
 
 function setup() {
-    // Create the canvas
-    createCanvas(880, 500);
-  
-    // Set angle mode to degrees
-    angleMode(DEGREES);
-  
-    // Set text color, size, and alignment
-    fill(255);
-    textSize(15);
-    textAlign(CENTER, CENTER);
-  
-    // Set the color mode to hue-saturation-brightness (HSB)
-    colorMode(HSB);
-  
-    // Create screen reader accessible description
-    describe('line segments rotated around center of canvas');
+  createCanvas(600, 400, WEBGL);
+
+  describe('A white torus on a gray background.');
+}
+function draw() {
+  background(400);
+
+  // Enable orbiting with the mouse.
+  orbitControl();
+  //mengatur warna / pencahayaan
+  if (gantiWarna === true) {
+    let colors = color('orange');
+    ambientLight(colors);
   }
-  
-  
-  function draw() {
-    // Clear the background
-    background(78);
-  
-    // Loop through angles 0, 30, 60, 90 degrees
-    for (let angle=10; angle <= 360; angle += 20) {
-        // Save current coordinate system
-        push();                       
-  
-        // Translate to center of canvas and rotate by angle
-        translate(width/2, height/2);
-        rotate(angle);
-  
-        // Set color based on angle and draw line along x-axis
-        stroke(angle+200, 100, 150,);
-        strokeWeight(5);
-        line(0, 0, 150, 0);
-  
-        // Display the angle
-        strokeWeight(3);              
-        text(angle, 170, 0);
-  
-        // Restore coordinate system
-        pop();                        
-    }
-  
-    // Draw the animated line
-    translate(width/2, height/2);
-    rotate(frameCount);
-    stroke(255);
-    strokeWeight(5);
-    line(0, 0, 150, 0);
-  }
+  // Draw the torus.
+  // Set its radius to 20 and tubeRadius to 15.
+  torus(100, 50);
+}
+// membuat fungsi agar sphere bisa berubah warna
+function doubleClicked() {
+  gantiWarna = true;
+}
